03ccc0dcf78f816d3a677f029f3ead18a867808d
Dương Tuấn Kiệt <duongtuankiet.dev@gmail.com>
2025-08-25 01:44:51 +0700

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const db = require('./src/models');
+const db = require('src/models');
 require('./utils/cronJob');
 
 const academicSyncService = require('./services/academicSyncService');
